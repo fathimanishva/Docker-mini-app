29ee03bac2b0d61a8f5565b2837c6e4c51e0a800
Nishva <fathimanishva7@gmail.com>
2025-11-14 00:17:24 +0530

    switch backend to cloud MongoDB

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://mongo:27017/dockerlearn', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
